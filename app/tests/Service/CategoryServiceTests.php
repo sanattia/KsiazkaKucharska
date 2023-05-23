cb1d05122449da59578649f00b1bd06c1bd80d8e
Agnieszka Moryc <40688807+sanattia@users.noreply.github.com>
2023-05-23 19:20:15 +0200

    Update CategoryServiceTests.php

--- app/tests/Service/CategoryServiceTests.php
+++ app/tests/Service/CategoryServiceTests.php
@@ -21,7 +21,6 @@ class CategoryServiceTests extends TestCase
 
     public function testConstructor(): bool
     {
-        // Create mock objects for the dependencies
         $categoryRepositoryMock = $this->getMockBuilder(CategoryRepository::class)
             ->disableOriginalConstructor()
             ->getMock();
@@ -29,17 +28,14 @@ class CategoryServiceTests extends TestCase
         $paginatorMock = $this->getMockBuilder(PaginatorInterface::class)
             ->getMock();
 
-        // Create an instance of the service with the mocked dependencies
         $categoryService = new CategoryService($categoryRepositoryMock, $paginatorMock);
 
-        // Additional assertions if necessary
 
         return true;
     }
 
     public function testCreatePaginatedList(): void
     {
-        // Create mock objects for the dependencies
         $categoryRepositoryMock = $this->getMockBuilder(CategoryRepository::class)
             ->disableOriginalConstructor()
             ->getMock();
@@ -47,10 +43,8 @@ class CategoryServiceTests extends TestCase
         $paginatorMock = $this->getMockBuilder(PaginatorInterface::class)
             ->getMock();
 
-        // Create an instance of the service with the mocked dependencies
         $categoryService = new CategoryService($categoryRepositoryMock, $paginatorMock);
 
-        // Mock the queryAll() method of the category repository
         $queryBuilderMock = $this->getMockBuilder(\Doctrine\ORM\QueryBuilder::class)
             ->disableOriginalConstructor()
             ->getMock();
@@ -59,7 +53,6 @@ class CategoryServiceTests extends TestCase
             ->method('queryAll')
             ->willReturn($queryBuilderMock);
 
-        // Mock the paginate() method of the paginator
         $paginationMock = $this->getMockBuilder(PaginationInterface::class)
             ->getMock();
 
@@ -68,11 +61,10 @@ class CategoryServiceTests extends TestCase
             ->with($queryBuilderMock, 1, CategoryRepository::PAGINATOR_ITEMS_PER_PAGE)
             ->willReturn($paginationMock);
 
-        // Call the method being tested
         $result = $categoryService->createPaginatedList(1);
 
-        // Assert the result
+  
         $this->assertInstanceOf(PaginationInterface::class, $result);
-        // Additional assertions if necessary
+  
     }
 }
